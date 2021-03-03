6f9407f8ff50df5248666fa7d91a2b7237b88187
jayaseelan.anbazhagan <jayaseelan.anbazhagan@learnship.com>
2021-03-03 23:53:59 +0530

    Init Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const chalk = require('chalk')
+const validator = require('validator')
+const yargs = require('yargs')
+
+/*const {userDetails, friendName} = require('./utils')
+
+
+
+fs.writeFileSync("userData.txt", "This content is written by arjun !!!  ")  // create if not exist 
+
+
+fs.appendFileSync("userData.txt", "This data is saved by jayaseelan via append method")  // create file if not exist
+
+
+const message = "Your notes saved successfully ..!"
+
+console.log(chalk.green.inverse(message))
+
+
+console.log(validator.isEmail('jayaseelangmail.com'))
+
+console.log(userDetails.age) */
+
+//console.log(process.argv)
+
+//console.log(yargs.argv)
+
+
+yargs.command({
+    command: 'add',
+    describe: 'To add new notes..!',
+    builder: {
+        title: {
+            describe: "This is note title",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function () {
+        console.log('notes added success')
+    },
+
+})
+
+yargs.parse()
---
+++ utils.js
@@ -0,0 +1,16 @@
+
+
+const userDetails = {
+    name: "Jayaseelan",
+    age: 31,
+    planet: 'Earth'
+
+}
+
+const friendName = "Ramesh"
+
+module.exports =
+{
+    userDetails,
+    friendName
+}
